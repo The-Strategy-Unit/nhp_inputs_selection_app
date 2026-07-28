55fe20933ce455e59ef88127129c4a93338e2240
Tom Jemmett <thomas.jemmett@nhs.net>
2026-07-28 12:15:48 +0100

    switch to use bslib

--- R/app_server.R
+++ R/app_server.R
@@ -166,11 +166,12 @@ app_server <- function(input, output, session) {
       unique() |>
       sort()
 
-    shiny::updateSelectInput(
+    shiny::updateSelectizeInput(
       session,
       "selected_user",
       choices = users,
-      selected = current_user()
+      selected = current_user(),
+      server = FALSE
     )
   })
 
@@ -214,7 +215,12 @@ app_server <- function(input, output, session) {
     )
     shiny::updateNumericInput(session, "seed", value = p$seed)
     shiny::updateSelectInput(session, "model_runs", selected = p$model_runs)
-    shiny::updateSelectInput(session, "app_version", selected = p$app_version)
+    shiny::updateSelectizeInput(
+      session,
+      "app_version",
+      selected = p$app_version,
+      server = FALSE
+    )
   }) |>
     shiny::bindEvent(params())
 
@@ -385,10 +391,11 @@ app_server <- function(input, output, session) {
   # 'create new' radio button should force model version dropdown to latest
   shiny::observe({
     if (input$scenario_type == "Create new from scratch") {
-      shiny::updateSelectInput(
+      shiny::updateSelectizeInput(
         session,
         "app_version",
-        selected = app_version_choices()[1]
+        selected = app_version_choices()[1],
+        server = FALSE
       )
     }
   }) |>
@@ -419,7 +426,7 @@ app_server <- function(input, output, session) {
 
       shiny::tags$a(
         "Start",
-        class = "btn btn-success",
+        class = "btn btn-primary",
         href = url
       )
     }
